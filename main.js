f394e4ad052453d4ab9ba40e8535cf4042c6ff2d
Şahin Arslan <sahin__arslan@hotmail.com>
2021-01-02 19:23:17 +0100

    basic react, webpack, babel and electron reload setup

--- main.js
+++ main.js
@@ -1,20 +1,52 @@
-const { app, BrowserWindow } = require('electron')
+// Main process
+const { app, BrowserWindow, Notification } = require('electron')
+const path = require('path')
+const isDev = !app.isPackaged // check if in dev or prod env
 
 // create window and load the index html
 function createWindow() {
+    // Renderer process
     const win = new BrowserWindow({
-        width: 800,
+        width: 1200,
         height: 800,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: false,
+            // will sanitize JS code to be safe
+            worldSafeExecuteJavascript: true,
+            // is a feature that ensures that both your
+            // preload scripts and Electron's internal logic tunes in seperate context:
+            contextIsolation: true,
         }
     })
 
     win.loadFile('index.html')
+    isDev && win.webContents.openDevTools()
+}
+
+// use electron reload in dev env
+if (isDev) {
+    require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+    })
 }
 
 // when app is ready, create the window
-app.whenReady().then(createWindow)
+app.whenReady()
+    .then(() => {
+        createWindow();
+        // const notification = new Notification({ title: 'Hello world', body: 'My test message' })
+        // notification.show()
+    })
 
+// callback method for main process
+app.on('window-all-closed', () => {
+    // if on windows and close the window, stop the app
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+// Webpack -> is a module builder, main purpose is to bundle JS files for usage in the browser.
+// Babel -> is a JS compiler
 
 console.log('hi world')
---
+++ src/js/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function App() {
+    const title = "Hello Saturn"
+    const enhancedTitle = title + " There - React App, perfect"
+
+    return (
+        <h1>{enhancedTitle}</h1>
+    )
+}
