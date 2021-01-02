e2e08e6a9d431ea403fcd886997cad1009387cf9
Şahin Arslan <sahin__arslan@hotmail.com>
2021-01-02 18:24:41 +0100

    introduce secure index html & createWindow

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+const { app, BrowserWindow } = require('electron')
+
+// create window and load the index html
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+// when app is ready, create the window
+app.whenReady().then(createWindow)
+
+
 console.log('hi world')
